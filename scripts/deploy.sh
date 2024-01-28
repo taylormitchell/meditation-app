1de2e6ede31488f9a7d83588005aa23870930365
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-01-28 15:52:38 -0500

    Update deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+set -e # Exit immediately if a command exits with a non-zero status (failure)
+
+# assert git status is clean
+if [[ $(git status -s) ]]; then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi 
+
+# build
+tsc && BASE_URL=/meditation-app/ vite build 
+
+# commit
+git add dist 
+git commit -m "Update dist"
+git push origin main
+
+# deploy
+git subtree push --prefix dist origin gh-pages
+
